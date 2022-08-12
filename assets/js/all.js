8e5d8132dc66971d19bb483af25f3ed2ea118f7a
ChenHelena <cocolulu2327@gmail.com>
2022-08-12 17:25:24 +0800

    Update 2022-08-12T09:25:15.156Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,22 +1,19 @@
-$(".reply").click(function(){
+"use strict";
+
+$(".reply").click(function () {
   $(".editor-wrap").show();
   $(".edit-before").hide();
-
 });
-$(".mark").click(function(){
+$(".mark").click(function () {
   $(".editor-wrap").hide();
   $(".edit-before").show();
-
 });
-
-$(".expand_more").click(function(){
+$(".expand_more").click(function () {
   $(".expand_more").hide();
   $(".expand_less").show();
-
 });
-$(".expand_less").click(function(){
+$(".expand_less").click(function () {
   $(".expand_more").show();
   $(".expand_less").hide();
-
 });
-
+//# sourceMappingURL=all.js.map
