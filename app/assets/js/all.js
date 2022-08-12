aa2f7b15dd4d88df73211b56de15092c1b1b0745
ChenHelena <cocolulu2327@gmail.com>
2022-08-12 17:23:33 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,22 @@
+$(".reply").click(function(){
+  $(".editor-wrap").show();
+  $(".edit-before").hide();
+
+});
+$(".mark").click(function(){
+  $(".editor-wrap").hide();
+  $(".edit-before").show();
+
+});
+
+$(".expand_more").click(function(){
+  $(".expand_more").hide();
+  $(".expand_less").show();
+
+});
+$(".expand_less").click(function(){
+  $(".expand_more").show();
+  $(".expand_less").hide();
+
+});
+
